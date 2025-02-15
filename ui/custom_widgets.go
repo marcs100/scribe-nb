8d888afe7b621a21e951ac77a5fec8ae3c05d649
marcs100 <marcs100@gmail.com>
2025-02-15 17:48:19 +0000

    added custom_widgets.go

---
+++ ui/custom_widgets.go
@@ -0,0 +1,29 @@
+package ui
+
+import (
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/widget"
+)
+
+type scribeNoteText struct {
+	widget.RichText
+	OnTapped func()
+}
+
+//Implement onTapped for this widget
+func (sn *scribeNoteText) Tapped(*fyne.PointEvent) {
+	if sn.OnTapped != nil {
+		sn.OnTapped()
+	}
+}
+
+func newScribeNoteText(content string, tapped func()) *scribeNoteText {
+	rt := &scribeNoteText{}
+	rt.AppendMarkdown(content)
+	rt.OnTapped = tapped
+	//rt.id = id
+	//rt.ExtendBaseWidget(rt)
+	return rt
+}
+
+
