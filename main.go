c3580ba0e0607008c4de245d4b3e6b063b674282
marcs100 <marcs100@gmail.com>
2025-04-11 16:29:13 +0100

    New light backgroung default colour

--- main.go
+++ main.go
@@ -88,7 +88,7 @@ func CreateAppConfig(homeDir string) config.Config {
 		DarkColourNote:   "#2f2f2f",
 		LightColourNote:  "#e2e2e2",
 		DarkColourBg:     "#1e1e1e",
-		LightColourBg:    "#e2e2e2",
+		LightColourBg:    "#f4f3fa",
 	}
 	newConfig := config.Config{
 		Title:    fmt.Sprintf("Scribe-nb v%s", VERSION),
